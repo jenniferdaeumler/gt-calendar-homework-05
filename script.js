b91d6701311955bd9694590525014d4d15a25206
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-13 14:37:37 -0500

    date added via moment

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+//Create date/time under heading
+//Create time blocks for hours of workday
+//Color blocks with past, present, future
+//Click on time block, type an event
+//Click save button to save
+//Saved in local storage
+
+
+//Create variables
+var date = moment().format("dddd, MMMM Do");
+console.log(date)
+
+//**Function definitions**//
+//
+$("#currentDay").text(date);
+
+//**Function calls**//
+
+
+//**Event listeners**//
+
+
+
 });
